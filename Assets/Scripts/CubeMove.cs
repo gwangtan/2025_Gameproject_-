3a7b145336a9f75d424ed49bd3833212b5c5e1bf
gwangtan <goms7401@gmail.com>
2025-04-09 15:59:25 +0900

    [feat] 6주차 폰트, 큐브게임

---
+++ Assets/Scripts/CubeMove.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeMove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float moveSpeed = 5.0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(0,0, - moveSpeed *  Time.deltaTime);
+
+
+        if (transform.position . z <  -20)
+        {
+            Destroy(gameObject);
+        }
+
+    }
+}
